02e0e3475c2af4d7f52ed605be77e4487a01dffd
James <jwin4740@gmail.com>
2017-02-13 21:15:35 -0500

    added spotify

--- liri.js
+++ liri.js
@@ -1,22 +1,57 @@
-var twitterObj = require("./keys.js");
-var Twitter = require('twitter');
-console.log("\n----------------------------------------");
-var twitterCreds = twitterObj.twitterKeys;
+// * `my-tweets`
 
-var client = new Twitter(twitterCreds);
+// * `spotify-this-song`
 
-var recentTweets = process.argv[2];
-var params = { screen_name: 'jwin4740' };
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-    	var numTweets = tweets[0].user.statuses_count;
-        if (recentTweets === "my-tweets") {
-            for (var i = 0; i < numTweets; i++) {
-                console.log("Tweet " + i + ": " + tweets[i].text);
-            }
-        } else {
-            console.log("please use the argument 'my-tweets'");
-        }
+// * `movie-this`
 
+// * `do-what-it-says`
+
+
+
+
+// var twitterObj = require("./keys.js");
+// var Twitter = require('twitter');
+// console.log("\n----------------------------------------");
+// var twitterCreds = twitterObj.twitterKeys;
+
+// var client = new Twitter(twitterCreds);
+
+// var recentTweets = process.argv[2];
+// var params = { screen_name: 'jwin4740' };
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if (!error) {
+//     	var numTweets = tweets[0].user.statuses_count;
+//         if (recentTweets === "my-tweets") {
+//             for (var i = 0; i < numTweets; i++) {
+//                 console.log("Tweet " + i + ": " + tweets[i].text);
+//             }
+//         } else {
+//             console.log("please use the argument 'my-tweets'");
+//         }
+
+//     }
+// });
+
+
+// node liri.js spotify-this-song '<song name here>'
+// * This will show the following information about the song in your terminal/bash window
+//     * Artist(s)
+//     * The song's name
+//     * A preview link of the song from Spotify
+//     * The album that the song is from
+
+// * if no song is provided then your program will default to
+//     * "The Sign" by Ace of Base
+
+var spotify = require('spotify');
+var song = process.argv[2];
+ 
+spotify.search({ type: 'track', query: song }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    else {
+        console.log(JSON.stringify(data, null, 2));
     }
 });
